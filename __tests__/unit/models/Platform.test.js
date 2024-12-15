86c257418e6ee9338ac539502a7a7cbb34d657d5
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-12-15 16:16:18 -0600

    Se agregaron las pruebas unitarias de Platform

--- __tests__/unit/models/Platform.test.js
+++ __tests__/unit/models/Platform.test.js
@@ -21,3 +21,122 @@ afterEach(async () => {
 afterAll(async () => {
   await database_test.close();
 });
+
+describe("Crear Platform", () => {
+  test("Debe crear una plataforma válida", async () => {
+    const platform = await PlatformModel.create(
+      {
+        name: "Steam Deck",
+      },
+      { transaction }
+    );
+
+    expect(platform).toBeTruthy();
+  });
+
+  test("No debe crear una plataforma sin nombre", async () => {
+    await expect(
+      PlatformModel.create(
+        {
+          // No se proporciona el nombre
+        },
+        { transaction }
+      )
+    ).rejects.toThrow();
+  });
+
+  test("No debe crear una plataforma con nombre duplicado", async () => {
+    await PlatformModel.create(
+      {
+        name: "Amazon Luna",
+      },
+      { transaction }
+    );
+
+    await expect(
+      PlatformModel.create(
+        {
+          name: "Amazon Luna",
+        },
+        { transaction }
+      )
+    ).rejects.toThrow();
+  });
+});
+
+describe("Buscar Platform", () => {
+  test("Debe encontrar una plataforma por nombre", async () => {
+    const platform = await PlatformModel.create(
+      {
+        name: "Nvidia Shield",
+      },
+      { transaction }
+    );
+
+    const foundPlatform = await PlatformModel.findOne({
+      where: { name: "Nvidia Shield" },
+      transaction,
+    });
+
+    expect(foundPlatform).toBeTruthy();
+  });
+
+  test("No debe encontrar una plataforma por nombre incorrecto", async () => {
+    const platform = await PlatformModel.findOne({
+      where: { name: "NonExistentPlatform" },
+      transaction,
+    });
+
+    expect(platform).toBeNull();
+  });
+});
+
+describe("Actualizar Platform", () => {
+  test("Debe crear y actualizar el nombre de una plataforma", async () => {
+    const platform = await PlatformModel.create(
+      {
+        name: "TemporaryPlatform",
+      },
+      { transaction }
+    );
+
+    expect(platform).toBeTruthy();
+
+    platform.name = "UpdatedPlatform";
+
+    await platform.save({ transaction });
+
+    const platformUpdated = await PlatformModel.findOne({
+      where: {
+        name: "UpdatedPlatform",
+      },
+      transaction,
+    });
+
+    expect(platformUpdated).toBeTruthy();
+  });
+});
+
+describe("Eliminar Platform", () => {
+  test("Debe eliminar una plataforma existente", async () => {
+    const platform = await PlatformModel.create(
+      {
+        name: "DeletablePlatform",
+      },
+      { transaction }
+    );
+
+    expect(platform).toBeTruthy();
+
+    await platform.destroy({ transaction });
+
+    const platformDeleted = await PlatformModel.findOne({
+      where: {
+        name: "DeletablePlatform",
+      },
+      transaction,
+    });
+
+    expect(platformDeleted).toBeNull();
+  });
+});
